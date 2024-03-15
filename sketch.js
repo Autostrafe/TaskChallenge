2bbc19a33b094f6f90a6f1823d27350b7c5cb669
Autostrafe <Levibautista360@gmail.com>
2024-03-15 11:04:45 -0600

    Button v1
    - Added button
    - Added Click on Button
    - Added Balls spawning when Button pressed
    - Added Background change on Button pressed

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let ballsArray = [];
-
+let ButtonState = false;
 function setup() {
  
 }
@@ -17,23 +17,25 @@ function draw() {
   background(40);
   rect(windowWidth/2-50,windowHeight/2-40,100,80);
   if (mouseIsPressed === true){
-    if (mouseX > windowWidth/2-100)
-    background(0);
-    fill(255,0,0);
- 
-
-    for (let i = 0; i < ballsArray.length; i++){
-      circle(ballsArray[i].x, ballsArray[i].y, ballsArray[i].diameter);
-      ballsArray[i].x += ballsArray[i].dx; // X-Rate of change
-      ballsArray[i].y += ballsArray[i].dy; // Y-Rate of Change
-  
+    if (ButtonState === false){
+      if (mouseX > windowWidth/2-50 && mouseX < (windowWidth/2-50 +100)){
+        if (mouseY > windowHeight/2-40 && mouseY < (windowHeight/2-40 + 80)){
+          background(0);
+          fill(random(255),random(255),random(255));
+          
+          for (let i = 0; i < ballsArray.length; i++){
+            circle(ballsArray[i].x, ballsArray[i].y, ballsArray[i].diameter);
+            ballsArray[i].x += ballsArray[i].dx; // X-Rate of change
+            ballsArray[i].y += ballsArray[i].dy; // Y-Rate of Change
+        
+          }
+        }
+      }
     }
   }
-
+  fill(255,255,255);
+  rect(windowWidth/2-50,windowHeight/2-40,100,80);
 }
-
-
-
 
 function createBall() { 
 
